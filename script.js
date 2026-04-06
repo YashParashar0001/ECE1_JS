27ea2deeea911b06c875f92a4a24cb6e828e3501
YashParashar0001 <parasharyash014@gmail.com>
2026-04-06 23:48:07 +0530

    getAttribute Method

---
+++ script.js
@@ -0,0 +1,150 @@
+// console.log('Hello World');
+
+
+// // Primitive DataTypes
+// let x;
+// console.log("X = ", x, "\t| TypeOf X: ", typeof(x));    // Undefined
+
+// x = 69;
+// console.log("X = ", x, "\t| TypeOf X: ", typeof(x));    // Number
+
+// x = "Hello JS";
+// console.log("X = ", x, "\t| TypeOf X: ", typeof(x));    // String
+
+// x = true;
+// console.log("X = ", x, "\t| TypeOf X: ", typeof(x));    // Boolean
+
+// x = null;
+// console.log("X = ", x, "\t| TypeOf X: ", typeof(x));    // Null
+
+
+// let sym1 = Symbol(4);
+// let sym2 = Symbol(4);
+
+// console.log(sym1, sym2)
+
+// (sym1 == sym2) ? console.log("Same") : console.log("Not Equal Symbol")
+
+
+
+
+// // FOR LOOP
+
+// document.write("<h1>FOR LOOP</h1></br>")
+// for (let count = 0; count < 6; count++) {
+//     console.log(count);
+//     document.write("Current Count: " + count + "</br>");
+// }
+
+
+
+// // FOR IN LOOP
+
+// document.write("<h1>FOR IN LOOP</h1>")
+// let person = {
+//     fname : "Rohan",
+//     lname : "Das",
+//     age : 25
+// }
+
+// console.log("Person Details:");
+// document.write("<h2>Person Details: </h2>");
+// for(let x in person){
+//     console.log( x + " : " + person[x]);
+//     document.write( x + " : " + person[x] + "</br>");
+// }
+
+
+// document.write("</br>");
+
+
+// // WHILE LOOP
+
+// document.write("<h1>WHILE LOOP</h1>")
+// var count = 0;
+// while (count < 6) {
+//     console.log(count);
+//     document.write("Current Count: " + count + "</br>");
+//     count++;
+// }
+
+
+
+
+// let age = 20;
+// if (age > 18){
+//     window.alert("Your age is : " + age + "\nYou are allowed to Drive a Car.");
+// }
+
+
+
+// if (age > 18){
+//     window.alert("Your age is : " + age + "\nYou are allowed to Drive a Car.");
+// }
+// else{
+//     window.alert("Your age is : " + age + "\nYou are a Minor, Can't Drive");
+// }
+
+
+
+
+
+
+// switch (age) {
+//     case 18:
+//         window.alert("You just Got 18! \nYou can Apply For Learning DL Now.")
+//         break;
+
+//     case 50:
+//         window.alert("You are 50! \nApply For New DL Now.")
+//         break;
+
+//     default:
+//         if (age > 18) {
+//             window.alert("Your age is : " + age + "\nYou are allowed to Drive a Car.");
+//         }
+//         else {
+//             window.alert("Your age is : " + age + "\nYou are a Minor, Can't Drive");
+//         }
+
+//         break;
+// }
+
+
+
+
+// function btnClicked() {
+//     window.alert("YOU CLICKED THE BUTTON !!!");
+//     console.log("Clicked Button!");
+// }
+
+
+// var myFunc = () =>{
+//     console.log("Hello");
+// }
+
+
+
+// // OBJECT
+
+// let person1 = {
+//     name: "Rohan das",
+//     age: 20,
+//     gender: "Male"
+// }
+
+// console.log(person1);
+
+
+
+// let person2 = new Object();
+// person2.name = "Shyam Singh";
+// person2['age'] = 34;
+// person2.gender = "Male";
+
+// console.log(person2);
+
+
+
+let myinfo = new Array();
+let myinfo1 = Array();
